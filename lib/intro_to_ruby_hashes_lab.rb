4c85c5e1316c05174707d750095cc15c4c38c3da
Esther Kang <estherkang14@gmail.com>
2020-05-18 20:03:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  update_counting_hash = {key => value}
+  update_counting_hash = {} 
   if hash[key]
     hash[key] += 1 
   else 
